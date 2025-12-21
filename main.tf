adffa4df95c9b158ad29d3d55571814f0410b9f9
Soda <akhowsuk@gmail.com>
2025-12-21 11:38:41 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,46 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
 resource "aws_key_pair" "main" {
-  key_name   = "harbour-space-devops-2025"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMaU0oWOzx21O0hnMWJs6LT1Bt2c2G9jDLkvNI/3MmQJ laborant@flexbox (managed)"
+  key_name   = "key-for-lab-laborant-01" 
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJtWOuiGt6NIkw5g34iDMg63umd9sgUXamI0OpZLZPUx laborant@flexbox (managed)"
+}
+
+resource "aws_security_group" "main" {
+  name        = "allow-ssh-and-4444-laborant"
+  description = "Allow SSH and port 4444"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 4444
+    to_port     = 4444
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "main" {
-  ami           = "ami-0006ba1ba3732dd33"
+  ami           = "ami-0006ba1ba3732dd33" # Ubuntu 20.04 (eu-central-1)
   instance_type = "t3.micro"
-  key_name = aws_key_pair.main.id
+  key_name      = aws_key_pair.main.key_name
+  
+  vpc_security_group_ids = [aws_security_group.main.id]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "My-AWS-Server"
   }
 }
